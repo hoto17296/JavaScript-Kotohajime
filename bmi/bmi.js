03cb402715eaf7f31c1d02c65b5e337fde74b060
hoto <hoto17296@icloud.com>
2014-06-08 16:09:46 +0900

    bmi answer

--- bmi/bmi.js
+++ bmi/bmi.js
@@ -1,6 +1,20 @@
 function showBMI() {
+  var h = document.BMIform.height.value;
+  var w = document.BMIform.weight.value;
 
-  // BMIを計算して表示する処理
+  if ( h == "" || w == "" ){
+    alert('身長と体重を入力してください');
+  } else {
+    var bmi = calcBMI(h, w);
+    document.getElementById('result').textContent = bmi;
+  }
 
   return false;
+}
+
+function calcBMI(h, w) {
+  h = h / 100; // 身長をcmからmに変換
+  var bmi = w / (h * h);
+  bmi = Math.round(bmi * 10) / 10; // 小数点第二位まで四捨五入
+  return bmi;
 }
